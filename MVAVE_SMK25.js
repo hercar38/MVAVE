1dead038c53f45cb3a7e1402cf0e3d65e69fc615
herve carret <hercartech38@gmail.com>
2025-02-08 00:10:38 +0100

    Add transport button

--- MVAVE_SMK25.js
+++ MVAVE_SMK25.js
@@ -17,6 +17,8 @@ var midiOutput = deviceDriver.mPorts.makeMidiOutput()
     
 deviceDriver.makeDetectionUnit().detectPortPair(midiInput, midiOutput)
 
+var surface = deviceDriver.mSurface
+
 //-----------------------------------------------------------------------------
 // 2. SURFACE LAYOUT - create control elements and midi bindings
 //-----------------------------------------------------------------------------
@@ -41,13 +43,56 @@ for(var channelIndex = 0; channelIndex < numChannels; ++channelIndex) {
 
     knobs.push(knob)
 }
+
+function makeTransport(x, y) {
+    var transport = {}
+
+    var w = 2
+    var h = 2
+
+    var currX = x
+
+    function bindMidiCC(button, chn, num) {
+        button.mSurfaceValue.mMidiBinding.setInputPort(midiInput).bindToControlChange(chn, num)
+    }
+
+    transport.btnStop = surface.makeButton(currX, y, w, h)
+    bindMidiCC(transport.btnStop, 0, 94)
+    currX = currX + w
+
+    transport.btnStart = surface.makeButton(currX, y, w, h)
+    bindMidiCC(transport.btnStart, 0, 93)
+    currX = currX + w
+
+    transport.btnRecord = surface.makeButton(currX, y, w, h)
+    bindMidiCC(transport.btnRecord, 0, 95)
+    currX = currX + w
+
+    return transport
+}
+
+function makeSurfaceElements() {
+    var surfaceElements = {}
+    surfaceElements.transport = makeTransport(1, 7)
+    return surfaceElements
+}
+
+var surfaceElements = makeSurfaceElements()
+
+function makeTransportDisplayFeedback(button, ledID, colorID) {
+	button.mSurfaceValue.mOnProcessValueChange = function (context, newValue) {
+		midiOutput.sendMidi(context, [ 0xbf, ledID, colorID * newValue ])
+	}
+}
+
+
 //-----------------------------------------------------------------------------
 // 3. HOST MAPPING - create mapping pages and host bindings
 //-----------------------------------------------------------------------------
 
 // create at least one mapping page
-var page = deviceDriver.mMapping.makePage('Example Mixer Page')
-var page = deviceDriver.mMapping.makePage('PadShop')
+//var page = deviceDriver.mMapping.makePage('Example Mixer Page')
+var page = deviceDriver.mMapping.makePage('Default')
 
 // create host accessing objects
 var hostSelectedTrackChannel = page.mHostAccess.mTrackSelection.mMixerChannel
@@ -57,5 +102,6 @@ for(var channelIndex = 0; channelIndex < numChannels; ++channelIndex) {
     var knobSurfaceValue = knobs[channelIndex].mSurfaceValue;
 
     // bind surface elements to host accessing object values
-    page.makeValueBinding(knobSurfaceValue, hostSelectedTrackChannel.mValue.mVolume)
+    // page.makeValueBinding(knobSurfaceValue, hostSelectedTrackChannel.mValue.mVolume)
+    page.makeValueBinding(knobSurfaceValue, hostSelectedTrackChannel.mSends.getByIndex(channelIndex).mLevel)
 }
